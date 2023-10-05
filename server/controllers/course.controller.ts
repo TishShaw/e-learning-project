1635ae518f28a57ae6d07ce937352550d356841a
Tishtanya Shaw <Tishtanyaa@icloud.com>
2023-10-04 21:27:47 -0400

    update course controller add notifications

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -9,6 +9,7 @@ import mongoose from 'mongoose';
 import ejs from 'ejs';
 import path from 'path';
 import sendMail from '../utils/sendMail';
+import NotificationModel from '../models/notificationModel';
 
 // Upload course
 export const uploadCourse = CatchAsyncerror(
@@ -208,6 +209,13 @@ export const addQuestion = CatchAsyncerror(
 			// Add question to course content
 			courseContent?.questions.push(newQuestion);
 
+			await NotificationModel.create({
+				user: req.user?._id,
+				title: 'New Question Reciebede',
+				message: `You have a new question from ${courseContent.title}
+            `,
+			});
+
 			// Save updated course
 			await course?.save();
 
@@ -270,6 +278,12 @@ export const addAnswer = CatchAsyncerror(
 
 			if (req.user?._id === question.user._id) {
 				// Notify user
+				await NotificationModel.create({
+					user: req.user?._id,
+					title: 'New Question Reply Recieved',
+					message: `You have a new question reply in ${courseContent.title}
+            `,
+				});
 			} else {
 				const data = {
 					name: question.user.name,
