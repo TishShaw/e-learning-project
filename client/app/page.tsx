803979b29837741831b30a1faa0d6461d8b0924c
Tishtanya Shaw <Tishtanyaa@icloud.com>
2023-10-15 16:00:36 -0400

    Add auth folder, update styles, layout and page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import Heading from './utils/Heading';
 import Header from './components/Header';
 import Hero from './components/Route/Hero';
@@ -9,6 +9,7 @@ interface Props {}
 const Page: FC<Props> = (props) => {
 	const [open, setOpen] = useState(false);
 	const [activeItem, setActiveItem] = useState(0);
+	const [route, setRoute] = useState('login');
 
 	return (
 		<div>
@@ -17,7 +18,13 @@ const Page: FC<Props> = (props) => {
 				description='ELearning is a platform for students to learn and get help from teachers'
 				keywords='Programming, MERN, Machine Learning'
 			/>
-			<Header open={open} setOpen={setOpen} activeItem={activeItem} />
+			<Header
+				open={open}
+				setOpen={setOpen}
+				activeItem={activeItem}
+				setRoute={setRoute}
+				route={route}
+			/>
 			<Hero />
 		</div>
 	);
